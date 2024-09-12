118ac467cb18468312a04a2d5ba2a4373231b00a
joncrall <erotemic@gmail.com>
2024-09-12 15:53:54 -0400

    Add API calls for log files

--- jellyfin_apiclient_python/api.py
+++ jellyfin_apiclient_python/api.py
@@ -631,6 +631,33 @@ class GranularAPIMixin:
             params['hasUserId'] = hasUserId
         return self._get("System/ActivityLog/Entries", params=params)
 
+    def get_log_file(self, name=None):
+        """
+        Args:
+            name (str | None):
+                The name of one of the logfiles returned by
+                `:func:get_server_logs`. If unspecified, the most
+                recently modified log will be returned.
+
+        Returns:
+            ...
+
+        References:
+            .. [GetLogFile] https://api.jellyfin.org/#tag/System/operation/GetLogFile
+        """
+        if name is None:
+            infos = self.get_server_logs()
+            if len(infos) == 0:
+                raise Exception('No system logs are available')
+            infos = sorted(infos, key=lambda d: d['DateModified'])
+            name = infos[-1]['Name']
+        result = self._get("System/Logs/Log", params={'name': name})
+        if result is None:
+            raise AssertionError(
+                ('FIXME: Result should not be none, but that seems to always '
+                 'be the case. Is this endpoint broken?'))
+        return result
+
     def post_capabilities(self, data):
         return self.sessions("/Capabilities/Full", "POST", json=data)
 
