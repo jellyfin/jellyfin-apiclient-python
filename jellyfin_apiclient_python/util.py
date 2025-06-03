d635dadb7a19dedd553e37258d0fe10e67ca4838
x12xx12x <44411062+12xx12@users.noreply.github.com>
2025-06-03 09:19:40 +0200

    Reduce cognitive complexity

--- jellyfin_apiclient_python/util.py
+++ jellyfin_apiclient_python/util.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 from typing import Any, Optional, Dict, Type, get_type_hints, List
 from uuid import UUID
 
@@ -10,24 +11,79 @@ def camel_to_snake(string: str) -> str:
     return string.lower()
 
 
-def api_parse_constructor(instance: Any, data: Optional[Dict] = None, child_classes: Optional[Dict[str, Type]] = None):
+def try_parse_child_classes(child_classes: Dict[str, Type], key: str, value: Any) -> Any:
+    """Parse child classes if they need further parsing."""
+    if key in child_classes:
+        if isinstance(value, List):
+            return [child_classes[key](item) for item in value]
+        else:
+            return child_classes[key](value)
+    return value
+
+def try_parse_uuid(key: str, value: Any, type_hints: Dict[str, Any]) -> Any:
+    """Parse a UUID from a string."""
+    if key in type_hints and type_hints[key] == Optional[UUID] and isinstance(value, str):
+        try:
+            return UUID(value) if value is not None else None
+        except ValueError as e:
+            raise ValueError(f"Failed to parse uuid: {value}, from {key}: {e}")
+    return value
+
+def try_parse_datetime(key: str, value: Any, type_hints: Dict[str, Any]) -> Any:
+    """Parse a datetime from an ISO format string."""
+    if key in type_hints and type_hints[key] == Optional[datetime]:
+        try:
+            time = datetime.fromisoformat(value.replace('Z', '+00:00'))
+            return time if value is not None else None
+        except ValueError as e:
+            raise ValueError(f"Failed to parse datetime: {value}, from {key}: {e}")
+    return value
+
+
+def api_parse_single_value(instance: Any, child_classes: Optional[Dict[str, Type]], key: str, value: Any, type_hints: Dict[str, Any]) -> None:
+    """
+    Parse a single value from an API response.
+    Args:
+        instance: The object to insert the data into.
+        child_classes: A dict of keys to classes if certain members need further parsing.
+        key: The key to parse - must be the name of the member
+        value: The value to insert
+        type_hints: Type hints for the instance for parsing UUID and datetime
+
+    Returns:
+        Nothing
+
+    Raises:
+        ValueError if the UUID isn't parsed correctly.
+    """
+    value = try_parse_child_classes(child_classes, key, value)
+    value = try_parse_uuid(key, value, type_hints)
+    value = try_parse_datetime(key, value, type_hints)
+    setattr(instance, key, value)
+
+def api_parse_constructor(instance: Any, data: Optional[Dict] = None, child_classes: Optional[Dict[str, Type]] = None) -> None:
+    """
+    Parse a response from API to the type of instance.
+    If the member is a UUID, it will be parsed to a UUID.
+    Args:
+        instance: The object to insert the data into.
+        data: The data to insert into the instance.
+        Keys are converted to snake_case to adhere to python naming standard.
+        child_classes: A dict of keys to classes if certain members need further parsing.
+
+    Returns:
+        Nothing
+
+    Raises:
+        KeyError if the provided object does not contain a member of the name.
+        ValueError if the UUID isn't parsed correctly.
+    """
     if data is not None:
         type_hints = get_type_hints(instance)
         for key, value in data.items():
             key = camel_to_snake(key)
-            if child_classes and key in child_classes:
-                if isinstance(value, List):
-                    setattr(instance, key, [child_classes[key](item) for item in value])
-                else:
-                    setattr(instance, key, child_classes[key](value))
-            elif hasattr(instance, key):
-                # Check if the field is expected to be a UUID
-                if key in type_hints and type_hints[key] == Optional[UUID]:
-                    try:
-                        setattr(instance, key, UUID(value) if value is not None else None)
-                    except ValueError as e:
-                        raise ValueError(f"{instance.__class__.__name__}Failed to parse uuid: {value}, from {key}: {e}")
-                else:
-                    setattr(instance, key, value)
-            else:
+
+            if not hasattr(instance, key):
                 raise KeyError(f"Key {key} is not available for {instance.__class__.__name__}")
+
+            api_parse_single_value(instance, child_classes, key, value, type_hints)
